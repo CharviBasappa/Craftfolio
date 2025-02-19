be89f098f1fdbf2852f3b46b32569780cd595a3d
Charvi Basappa <charvibasappa98@gmail.com>
2025-02-18 19:29:59 -0800

    Added Navbar component with a menu

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <nav className="nav">
+      <h1>My Portfolio</h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/skills">Skills</Link>
+        </li>
+        <li>
+          <Link to="/projects">Projects</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
