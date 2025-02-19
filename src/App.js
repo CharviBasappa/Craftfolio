763f8a0c3686d70baa53bc74613354e760f0143b
Charvi Basappa <charvibasappa98@gmail.com>
2025-02-18 20:25:31 -0800

    Just added one project to showcase on web for now

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
+import Projects from "./pages/Projects";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/skills" element={<Skills />} />
+        <Route path="/projects" element={<Projects />} />
       </Routes>
       <Footer />
     </Router>
---
+++ src/pages/Projects.js
@@ -0,0 +1,12 @@
+const Projects = () => {
+  return (
+    <div>
+      <h1>Projects</h1>
+      <ul>
+        <li>MunchMate - Food Ordering App</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Projects;
