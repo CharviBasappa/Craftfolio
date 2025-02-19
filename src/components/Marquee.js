9ec07dfdd0d08286f991ce5cb34aae1a9bb9271d
Charvi Basappa <charvibasappa98@gmail.com>
2025-02-18 19:38:53 -0800

    Created Marquee and added Home page

---
+++ src/components/Marquee.js
@@ -0,0 +1,15 @@
+import Marquee from "react-fast-marquee";
+
+const Marquee = () => {
+  return (
+    <Marquee speed={50} gradient={false}>
+      <p style={{ marginRight: "40px" }}>React.js</p>
+      <p style={{ marginRight: "40px" }}>TypeScript</p>
+      <p style={{ marginRight: "40px" }}>Redux</p>
+      <p style={{ marginRight: "40px" }}>Node.js</p>
+      <p style={{ marginRight: "40px" }}>Python</p>
+    </Marquee>
+  );
+};
+
+export default Marquee;
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import Marquee from "../components/Marquee";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>Full-Stack Developer | Python & React</p>
+      <Marquee />
+    </div>
+  );
+};
+
+export default Home;
